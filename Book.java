b51520977aa2f24bd629ea90cf657392eb225390
aryder405 <aryder405@gmail.com>
2013-09-03 16:02:20 -0600

    Book object

---
+++ Book.java
@@ -0,0 +1,103 @@
+
+public class Book {
+	
+	private String name="", author="";
+	private int avgWordLength=-1,avgSentenceLength=-1,wordToSentenceRatio=-1,letterFreq=-1,
+			letterPairFreq=-1,vocabRichness=-1,freqNoun=-1,freqVerb=-1,freqAdjective=-1,adjectiveToNounRatio=-1,
+			lexicalDensity=-1,wordLength=-1;
+	
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String value){
+		this.name = value;
+	}
+	public String getAuthor(){
+		return this.author;
+	}
+	public void setAuthor(String value){
+		this.author = value;
+	}
+	public int getAvgSentenceLength(){
+		return this.avgSentenceLength;
+	}
+	public void setAvgSentenceLength(int value){
+		this.avgSentenceLength = value;
+	}
+	public int getWordToSentenceRatio(){
+		return this.wordToSentenceRatio;
+	}
+	public void setWordToSentenceRatio(int value){
+		this.wordToSentenceRatio = value;
+	}
+	public int getAvgWordLength(){
+		return this.avgWordLength;
+	}
+	public void setAvgWordLength(int value){
+		this.avgWordLength = value;
+	}
+	public int getLetterFreq(){
+		return this.letterFreq;
+	}
+	public void setLetterFreq(int value){
+		this.letterFreq = value;
+	}
+	public int getLetterPairFreq(){
+		return this.letterPairFreq;
+	}
+	public void setLetterPairFreq(int value){
+		this.letterPairFreq = value;
+	}
+	public int getVocabRichness(){
+		return this.vocabRichness;
+	}
+	public void setVocabRichness(int value){
+		this.vocabRichness = value;
+	}
+	public int getFreqNoun(){
+		return this.freqNoun;
+	}
+	public void setFreqNoun(int value){
+		this.freqNoun = value;
+	}
+	public int getFreqVerb(){
+		return this.freqVerb;
+	}
+	public void setFreqVerb(int value){
+		this.freqVerb = value;
+	}
+	public int getFreqAdjective(){
+		return this.freqAdjective;
+	}
+	public void setFreqAdjective(int value){
+		this.freqAdjective = value;
+	}
+	public int getAdjectiveToNounRatio(){
+		return this.adjectiveToNounRatio;
+	}
+	public void setAdjectiveToNounRatio(int value){
+		this.adjectiveToNounRatio = value;
+	}
+	public int getLexicalDensity(){
+		return this.lexicalDensity;
+	}
+	public void setLexicalDensity(int value){
+		this.lexicalDensity = value;
+	}
+	public int getWordLength(){
+		return this.wordLength;
+	}
+	public void setWordLength(int value){
+		this.wordLength = value;
+	}
+	
+	
+
+	
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
